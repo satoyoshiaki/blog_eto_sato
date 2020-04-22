a03d61f74651d9ad7033349ee58502e40918dce3
佐藤良昭 <yoshiprograming@gmail.com>
2020-04-22 16:24:57 +0900

    お気に入り機能の実装とアソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :blogs
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources :favorites, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
